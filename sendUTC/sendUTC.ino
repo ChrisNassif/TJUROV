f282e044f85f7db4a8fd023a468e272e47a779a8
Animated <chrisjnassif@gmail.com>
2023-03-29 15:06:12 -0400

    Init

---
+++ sendUTC/sendUTC.ino
@@ -0,0 +1,16 @@
+#include <WiFi.h>
+void setup() {
+  Serial.begin(9600)
+
+}
+
+void loop() {
+  time_t UTC = now();
+}
+
+
+// DEPENDENCIES: (install in the following order)
+
+// Time Library in library manager
+// https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
+// install the esp32 board in tools/ board manager
