458eace5c487d4b686cedca21bac5c4ec8408fb0
Animated <chrisjnassif@gmail.com>
2023-03-30 14:09:32 -0400

    Forgot to commit this part oops

--- GroundStationFloat/GroundStationFloat.py
+++ GroundStationFloat/GroundStationFloat.py
@@ -4,9 +4,8 @@ import time
 # HOW TO USE:
 # 1. Start the ESP32 script
 # 2. Connect to ESP32's wifi signal through the ground station
-# 3. Get the ESP32's IP Address and write it down in ESP32_IPADDRESS
-# 4. (optional) Make sure you can access the ESP32's signal by typing in [ESP32_IPADDRESS]/UTC_Time into google
-# 5. Run this script
+# 3. (optional) Make sure you can access the ESP32's signal by typing in [ESP32_IPADDRESS]/UTC_Time into google
+# 4. Run this script
  
 ESP32_IPADDRESS = "192.168.1.184"
 
