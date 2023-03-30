3ad6574d539467b6238504dfbd344fa93b53899f
Animated <chrisjnassif@gmail.com>
2023-03-29 21:47:46 -0400

    Finished Rough Draft of UTC Time Stuff

---
+++ GroundStationFloat/GroundStationFloat.py
@@ -0,0 +1,22 @@
+import requests
+import time
+
+# HOW TO USE:
+# 1. Start the ESP32 script
+# 2. Connect to ESP32's wifi signal through the ground station
+# 3. Get the ESP32's IP Address and write it down in ESP32_IPADDRESS
+# 4. (optional) Make sure you can access the ESP32's signal by typing in [ESP32_IPADDRESS]/UTC_Time into google
+# 5. Run this script
+ 
+ESP32_IPADDRESS = ""
+
+def main():
+
+    while True:
+        # I am pretty sure that it needs the https out front but not certain
+        result = requests.get(f'https://{ESP32_IPADDRESS}/UTC_Time')
+        print(result)
+        time.sleep(500)
+
+if __name__ == "__main__": main()
+    
--- sendUTC/sendUTC.ino
+++ sendUTC/sendUTC.ino
@@ -1,11 +1,45 @@
-#include <WiFi.h>
-void setup() {
-  Serial.begin(9600)
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
 
+const char* ssid = "UROSUS";
+const char* password = NULL;
+
+// Set web server port number to 80
+AsyncWebServer server(80);
+
+IPAddress IP;
+
+time_t get_UTC_Time() {
+  return now()
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.mode(WIFI_AP)
+  Serial.print("Setting AP (Access Point)…");
+  WiFi.softAP(ssid, password);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Setting AP (Access Point)…");
+  WiFi.softAP(ssid, password);
+
+  IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.on("/UTC_Time", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", get_UTC_Time().to_string());
+  });
+
+  server.begin()
+  
 }
 
 void loop() {
-  time_t UTC = now();
+  
+
+
 }
 
 
@@ -14,3 +48,18 @@ void loop() {
 // Time Library in library manager
 // https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
 // install the esp32 board in tools/ board manager
+// download the following and add it with [Sketch/Include Library/Add .ZIP Library]: 
+//    https://github.com/me-no-dev/ESPAsyncWebServer/archive/master.zip
+
+
+
+// RESOURCES:
+// https://randomnerdtutorials.com/esp32-access-point-ap-web-server/#:~:text=ESP32%20IP%20Address,ESP32%20point%20will%20be%20printed.
+
+
+
+// OTHER MISC RESOURCES THAT COULD BE USEFULL LATER:
+// If connection through https doesn't work then:
+// https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFi/src/WiFiUdp.h
+// https://gist.github.com/santolucito/4016405f54850f7a216e9e453fe81803
+
